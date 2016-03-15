38b4524d99bd22107d0668524580dcab62fba582
Erica Lee <ericanylee@gmail.com>
2016-03-15 01:38:12 -0500

    Facebook Frameworks Imported

---
+++ ProjectBuddies/Header.h
@@ -0,0 +1,16 @@
+//
+//  Header.h
+//  ProjectBuddies
+//
+//  Created by Erica Lee on 3/15/16.
+//  Copyright © 2016 EricaAndAini. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+#endif /* Header_h */
---
+++ ProjectBuddies/ProjectBuddies-Header.h
@@ -0,0 +1,16 @@
+//
+//  ProjectBuddies-Header.h
+//  ProjectBuddies
+//
+//  Created by Erica Lee on 3/15/16.
+//  Copyright © 2016 EricaAndAini. All rights reserved.
+//
+
+#ifndef ProjectBuddies_Header_h
+#define ProjectBuddies_Header_h
+
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* ProjectBuddies_Header_h */
